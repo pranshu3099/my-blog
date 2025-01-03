b306ec30750c16e1885ad8c52f69d737bf918615
pranshu <srivastavapranshu2@gmail.com>
2025-01-04 01:33:31 +0530

    get a short description for your blog

--- utility/getimage.js
+++ utility/getimage.js
@@ -1,4 +1,4 @@
-export default function extractFirstImageSrc(htmlString) {
+export function extractFirstImageSrc(htmlString) {
   if (!htmlString || typeof htmlString !== "string") {
     return null;
   }
@@ -7,4 +7,25 @@ export default function extractFirstImageSrc(htmlString) {
   );
 
   return regexMatch ? regexMatch[1] : null;
+}
+
+export function extractFirstParagraphText(htmlString) {
+  if (!htmlString || typeof htmlString !== "string") {
+    return null;
+  }
+
+  const regexMatch = htmlString.match(/<p[^>]*>([^<]+)<\/p>/i);
+
+  if (!regexMatch) {
+    return null;
+  }
+  const fullText = regexMatch[1].trim();
+  const firstLine = fullText.split(/[\n\r]+/)[0];
+  if (firstLine.length > 100) {
+    const truncated = firstLine.substr(0, 100).split(" ");
+    truncated.pop();
+    return truncated.join(" ") + "...";
+  }
+
+  return firstLine;
 }
