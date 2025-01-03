b81fd333fd0e447195d08c81e149d058d5ca990a
pranshu <srivastavapranshu2@gmail.com>
2025-01-04 02:00:27 +0530

    use &quot; instead of  ""

--- components/projects/index.jsx
+++ components/projects/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 const MyProjects = () => {
   return (
     <>
@@ -7,42 +8,43 @@ const MyProjects = () => {
         </div>
         <div className="flex flex-col gap-4 mt-4 sm:p-0 p-2 ">
           <div className="flex flex-col gap-2 h-auto">
-            <a
+            <Link
               href="https://bro-streams.vercel.app/"
               target="_blank"
               className="font-bold sm:text-[25px] text-[70px]"
             >
               Bro Streams
-            </a>
+            </Link>
             <p className="sm:text-[25px] text-[50px]">
-              Developed "Bro Streams," a Twitch-like platform using Next.js 14
-              with Clerk authentication. Real-time streaming via LiveKit,
-              Tailwind CSS for styling, and interactive user features like
-              following, blocking, and guest access.
+              Developed &quot;Bro Streams,&quot; a Twitch-like platform using
+              Next.js 14 with Clerk authentication. Real-time streaming via
+              LiveKit, Tailwind CSS for styling, and interactive user features
+              like following, blocking, and guest access.
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <a
+            <Link
               href="https://chat.pranshu.dev/"
               target="_blank"
               className="font-bold sm:text-[25px] text-[70px]"
             >
               Doge Chat
-            </a>
+            </Link>
             <p className="sm:text-[25px] text-[50px]">
-              Built a fun, real-time chat application featuring a "Doge" dog
-              using React.js, Node.js, and OpenAI, with secure OTP-based
-              authentication and WebSocket for live chat, deployed on AWS.
+              Built a fun, real-time chat application featuring a
+              &quot;Doge&quot; dog using React.js, Node.js, and OpenAI, with
+              secure OTP-based authentication and WebSocket for live chat,
+              deployed on AWS.
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <a
+            <Link
               href="https://pranshu3099.github.io/movies-hub/index.html"
               target="_blank"
               className="font-bold sm:text-[25px] text-[70px]"
             >
               Movies Hub
-            </a>
+            </Link>
             <p className="sm:text-[25px] text-[50px] ">
               Search for movies and tv series.
             </p>
