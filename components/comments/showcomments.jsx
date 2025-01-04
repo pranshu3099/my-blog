f2c03a86d0b3d018a377c6df736afbf4439cf6e1
pranshu <srivastavapranshu2@gmail.com>
2025-01-04 06:49:56 +0530

    get comments

--- components/comments/showcomments.jsx
+++ components/comments/showcomments.jsx
@@ -1,5 +1,6 @@
 const ShowComments = ({ commentList }) => {
-  if (commentList.length === 0) {
+  console.log(commentList);
+  if (commentList?.length === 0) {
     return (
       <div className="h-auto mx-auto sm:w-full max-w-[1500px] w-[1200px] p-10 font-bold">
         <p className="h-auto sm:text-[25px] text-[50px]">
