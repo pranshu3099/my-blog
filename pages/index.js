cf5e2363f9a2c567accaba966bc69a2020b6bbd5
pranshu <srivastavapranshu2@gmail.com>
2025-01-05 23:15:37 +0530

    added meta tags for home page

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,21 @@ import Footer from "@/components/footer";
 import Head from "next/head";
 import ErrorBoundary from "@/components/error";
 export default function Home({ posts }) {
+  const currentUrl = `${process.env.NEXT_PUBLIC_URL}`;
   return (
     <div>
       <Head>
         <title>Pranshu&apos;s Blog</title>
+        <meta property="og:title" content="Pranshu's Blog" />
+        <meta
+          property="og:description"
+          content="Hi there, I'm Pranshu, aka Brocode! I'm a 24-year-old Software Engineer with a passion for building web applications."
+        />
+        <meta property="og:url" content={`${currentUrl}`} />
+        <meta
+          property="og:image"
+          content="https://hxwnfkyekkzeimdyyksm.supabase.co/storage/v1/object/public/images/broblogsimages/blue_lake_5-wallpaper-2048x768.jpg"
+        />
       </Head>
       <Navbar />
       <div className="h-[710px] mx-auto w-[700px] sm:w-full sm:max-w-[1500px]">
