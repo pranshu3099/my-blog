498c26d4c64a7fd39f4c9dcd7b3e56603d9958af
pranshu <srivastavapranshu2@gmail.com>
2025-03-11 22:59:10 +0530

    wrap whole app with AuthProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/context/provider";
+import { AuthProvider } from "@/context/authprovider";
 import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Toaster position="bottom-center" />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <Toaster position="bottom-center" />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
