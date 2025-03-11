1861147770e24204f21030da6aa93548f8e19dbb
pranshu <srivastavapranshu2@gmail.com>
2025-03-11 22:51:22 +0530

    get your current age

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -1,4 +1,5 @@
 import Carousel from "../carousel";
+import { getMyCurrentAge } from "@/utility/slugify";
 const About = () => {
   return (
     <>
@@ -8,14 +9,15 @@ const About = () => {
             Hi i&apos;m Pranshu
           </h1>
           <p className="p-2 sm:text-[25px] text-[50px] sm:w-[600px]">
-            Hi there, I&apos;m Pranshu, aka Brocode! I&apos;m a 24-year-old
-            Software Engineer with a passion for building web applications. I
-            mostly work on web-related projects, using technologies like
-            Next.js, React, and Node.js. Recently, I started writing articles on
-            my blog to share the things I learn along the way. When I&apos;m not
-            coding, you can find me exploring new tech, brainstorming fun side
-            projects, or diving into system design concepts. My blog is a mix of
-            my learnings, experiences, and tips for fellow developers.
+            Hi there, I&apos;m Pranshu, aka Brocode! I&apos;m a{" "}
+            {getMyCurrentAge()}-year-old Software Engineer with a passion for
+            building web applications. I mostly work on web-related projects,
+            using technologies like Next.js, React, and Node.js. Recently, I
+            started writing articles on my blog to share the things I learn
+            along the way. When I&apos;m not coding, you can find me exploring
+            new tech, brainstorming fun side projects, or diving into system
+            design concepts. My blog is a mix of my learnings, experiences, and
+            tips for fellow developers.
           </p>
         </div>
         <div className="p-10 w-full items-center justify-center sm:flex hidden">
--- utility/slugify.js
+++ utility/slugify.js
@@ -1,7 +1,22 @@
+const MY_DOB = "2000-07-02";
 export const slugify = (text) => {
   return text
     .toString()
     .toLowerCase()
     .trim()
     .replace(/[\s\W-]+/g, "-");
+};
+
+export const getMyCurrentAge = () => {
+  const birthMonth = new Date(MY_DOB).getMonth() + 1;
+  const birthYear = new Date(MY_DOB).getFullYear();
+  const currentMonth = new Date("2025-03-11").getMonth() + 1;
+  const currentYear = new Date("2025-03-11").getFullYear();
+  let years = currentYear - birthYear;
+  let months = currentMonth - birthMonth;
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+  return years;
 };
